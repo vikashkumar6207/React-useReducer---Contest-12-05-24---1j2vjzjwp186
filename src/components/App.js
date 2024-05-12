2d89c22851ab1d4a505a644bedc203a32f30e6b4
vikashkumar6207 <vktd828403@gmail.com>
2024-05-12 19:50:39 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import '../styles/App.css';
-import { useReducer } from 'react';
 
 const ACTIONS = {
   INCREMENT: "increment",
@@ -8,6 +7,7 @@ const ACTIONS = {
   SET_SUB_NUM: "setSubNum",
   SET_ADD_NUM: "setAddNum"
 };
+
 function reducer(state, action) {
   switch (action.type) {
     case ACTIONS.INCREMENT:
@@ -16,14 +16,28 @@ function reducer(state, action) {
         addNum: state.addNum,
         subNum: state.subNum
       };
- 
-   
+
+    case ACTIONS.DECREMENT:
+      return {
+        count: Number(state.count) - Number(state.subNum),
+        addNum: state.addNum,
+        subNum: state.subNum
+      };
+
     case ACTIONS.SET_ADD_NUM:
       return {
         count: state.count,
         subNum: state.subNum,
         addNum: action.payload
       };
+
+    case ACTIONS.SET_SUB_NUM:
+      return {
+        count: state.count,
+        subNum: action.payload,
+        addNum: state.addNum
+      };
+
     default:
       return {
         count: 10,
@@ -34,7 +48,11 @@ function reducer(state, action) {
 }
 
 const App = () => {
- 
+  const [state, dispatch] = useReducer(reducer, {
+    count: 10,
+    subNum: 1,
+    addNum: 1
+  });
 
   function onIncrement() {
     dispatch({ type: ACTIONS.INCREMENT });
@@ -64,6 +82,5 @@ const App = () => {
     </div>
   )
 }
-
 
 export default App;
